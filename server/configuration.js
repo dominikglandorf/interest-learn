752e1e2d07aa5954934f7b05ff3c2a2275bc8885
Dominik Glandorf <do.gl@gmx.de>
2023-09-16 14:41:38 -0400

    fix db configuration

--- server/configuration.js
+++ server/configuration.js
@@ -5,5 +5,6 @@ const openai = new OpenAI();
 
 MODEL = process.env.MODEL
 MOCK = process.env.MOCK == "1"
+DB_NAME = process.env.DB_NAME
 
-module.exports = { openai, MODEL, MOCK }
+module.exports = { openai, MODEL, MOCK, DB_NAME }
--- server/db/connect.js
+++ server/db/connect.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose');
-const { DB_NAME } = require('./configuration');
+const { DB_NAME } = require('../configuration');
 
 mongoose.connect('mongodb://127.0.0.1:27017/' + DB_NAME);
